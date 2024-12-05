3296436b152a336004ffdfd0214081bf963c51a4
lautaro122772 <lautaroWW123@gmail.com>
2024-12-05 18:49:05 -0300

    mi primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_leguizamon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
