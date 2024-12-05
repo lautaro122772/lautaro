89ea4edbdb02a06bd885b6dedb2728d3170c4fc6
lautaro122772 <lautaroWW123@gmail.com>
2024-12-05 19:14:00 -0300

    ejersicio de multriplicacion

--- Program.cs
+++ Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Tp_leguizamon
 {
-    class Program
+    public class TablasMultiplicarInfinitas
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i <= y; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("{0}x{1}= {2}", i, j, i * j);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
         }
     }
 }
+
+/*
+ * Cree un programa en C# que solicite un rango de números al usuario (x,y) y muestre las tablas de multiplicar de x hasta y.
+ * */
